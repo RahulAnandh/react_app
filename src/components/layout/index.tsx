3a89453f6a4e868d4b2bf5c4a2172932bce0b00e
RahulAnandh <rahulanandh80@gmail.com>
2024-10-07 18:42:20 +0530

    side bar removed navbar folder

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,28 @@
 import "./index.css";
-import NavBarIndex from "../nav_bar";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import nav_bar_data from "./nav_bar_data";
+import "./index.css";
 
 const LayoutIndex = () => {
   return (
     <div className="layout_page">
       <div className="side_bar">
-        <NavBarIndex />
-      </div>
+      <div>
+      <nav>
+      <ul>
+      {nav_bar_data.map((obj, key) => {
+        return (
+          <li>
+          <div className="side_bar_row" key={key} >
+            <div id="icon">{obj.icon}</div>
+            <div id="title">{obj.title}</div>
+          </div>
+        </li>
+        );
+      })}
+      </ul>
+      </nav>
+    </div>      </div>
       <div className="page_content"><Outlet/></div>
     </div>
   );
